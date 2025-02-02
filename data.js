1dd657725d8224aa06527129fccadbfab53f4f90
fky101 <96272954+fky101@users.noreply.github.com>
2025-02-02 22:04:50 +0700

    add and update entries

--- data.js
+++ data.js
@@ -1,10 +1,24 @@
+import suratThaniImg from "./src/images/suratthani.jpg"
 import chiangMaiImg from "./src/images/chiangmai.jpg"
+import chiangRaiImg from "./src/images/chiangrai.jpg"
 import japanImg from "./src/images/japan.jpg"
 import englandImg from "./src/images/england.jpg"
 
 export default [
     {
-        id: 1,
+        id: 5,
+        img: {
+            src: suratThaniImg,
+            alt: "a person standing in boat in front of "
+        },
+        title: "Khao Sam Kler",
+        country: "Thailand",
+        googleMapsLink: "https://maps.app.goo.gl/4B22DHuHyoxDtMQDA",
+        dates: "12 June, 2024 - 17 June, 2024",
+        text: "Khao Sam Kler consists of three limestone peaks standing side by side in the middle of the Cheow Lan Lake, located within Khao Sok National Park in Surat Thani."
+    },
+    {
+        id: 4,
         img: {
             src: chiangMaiImg,
             alt: "A person standing at a scenic viewpoint along the Kew Mae Pan Nature Trail"
@@ -14,6 +28,18 @@ export default [
         googleMapsLink: "https://maps.app.goo.gl/fZ5SYxTKQnDbKdQq8",
         dates: "26 Dec, 2023 - 29 Dec, 2023",
         text: "Kew Mae Pan nature trail is a hiking and trekking trail located inside the area of Doi Inthanon National Park, home to Thailand’s highest peak."
+    },
+    {
+        id: 3,
+        img: {
+            src: chiangRaiImg,
+            alt: "a person standing at the top of Phu Chi Fa during sunrise"
+        },
+        title: "Phu Chi Fa",
+        country: "Thailand",
+        googleMapsLink: "https://maps.app.goo.gl/F3QTawpv6pXs93rB6",
+        dates: "12 Dec, 2023 - 14 Dec, 2023",
+        text: "Phu Chi Fa is one of the most famous destinations uphill in Chiang Rai, offering spectacular views of the valley and sea of mist at sunrise."
     },
     {
         id: 2,
@@ -28,10 +54,10 @@ export default [
         text: "Asahiyama Zoo is Japan's northernmost zoo, famous for its interactive exhibits that bring visitors closer to animals like polar bears, penguins, and seals."
     },
     {
-        id: 3,
+        id: 1,
         img: {
             src: englandImg,
-            alt: "Geirangerfjord"
+            alt: "A person in front of Anfield stand"
         },
         title: "Anfield",
         country: "England",
